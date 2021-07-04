b18706346e5bd28aa8c925ea84c638c690bb4194
hamed <hamediran1999@gmail.com>
2021-07-04 17:18:21 +0430

    Add project files.

---
+++ WebApplication1/Controllers/hello.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    //
+    // GET : /hello/
+    public class Hello : Controller
+    {
+        public string Index()
+        {
+            return "hi its me";
+        }
+        public string Wel()
+        {
+            return "this is well";
+        }
+    }
+}
