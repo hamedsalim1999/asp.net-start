6f8e89462682771fd34b661b96670785820240b5
hamed <hamediran1999@gmail.com>
2021-07-04 17:22:01 +0430

    change control name

--- WebApplication1/Controllers/hello.cs
+++ WebApplication1/Controllers/hello.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Controllers
+namespace MovieApp.Controllers
 {
     //
     // GET : /hello/
